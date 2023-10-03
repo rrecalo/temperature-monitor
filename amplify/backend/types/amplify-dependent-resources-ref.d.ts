c019e9d32598b877c57bc3d0b7bf995a386f961a
Rrazmik <robertrecalo@icloud.com>
2023-10-03 19:17:23 -0400

    Initialize S3 using aws-amplify cli

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "temperaturemonitor26ab0012": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}

a47ea71dc7694e5a41ef0f6f6eb0475f88c6a5a7
Rrazmik <robertrecalo@icloud.com>
2023-10-03 20:33:53 -0400

    Save mock data in data.json file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import data from './data.json';
 
 function App() {
+  const [files, setFiles] = useState();
+
+  useEffect(()=>{
+    //getData().then(data=>{console.log(data)});
+    setFiles(data);
+  },[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }

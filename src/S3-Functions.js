230a713bb74f2bb17c743bcf182ecbe308b36c4c
Titus-Filimon <146393880+Titus-Filimon@users.noreply.github.com>
2023-10-31 12:41:49 -0400

    Code cleanup (#2)
    * Modified formatUnixTimestamp
    
    * S3-Functions.js
    
    Changed if...else error handling to try...catch and added shorthands for small if...else statements
    
    * Update App.js
    
    Did some cleaning.
    
    ( line 14) In a loose equality ( == ) null=undefined and vice verse since a loose equality doesn't check for the type of value. In a strict equality ( === ) null=/=undefined since they are 2 different types. Undefined is a type in itself while null is an object. Check console.log( typeof(null) )
    
    Shorthand on line 86 doesn't need to be on multiple lines.

--- src/S3-Functions.js
+++ src/S3-Functions.js
@@ -5,13 +5,10 @@ import { Storage } from 'aws-amplify';
 async function getAllObjectKeys(objectCount){
     const request = await Storage.list('esp32/pub/', {pageSize: objectCount});
     let keys = [];
-    if(request.results){
-        request.results.forEach((obj) => {
-            keys.push(obj.key);
-        });
-    }
-    else{
-        console.log("Error listing data from S3 bucket...");
+    try {
+        if (request.results) request.results.forEach((obj) => {keys.push(obj.key)});
+    } catch(err){
+        console.error("Error listing data from S3 bucket...", err);
     }
     //we reverse so that the newest added object in s3 is listed first, instead of the default (oldest first)
     keys.reverse();
@@ -22,20 +19,14 @@ async function getObjectByKey(objectKey){
     //file url is the download URL of the file that has that key in the S3 bucket
     var fileUrl = await Storage.get(objectKey, {download : false});
     var fileResponse = await fetch(fileUrl);
-    if(fileResponse.ok){
-        return fileResponse.json();
-    }
+    if (fileResponse.ok) return fileResponse.json();
 }
 
 //Parameter objectCount should be of type number - or not passed at all
 async function getData(objectCount){
     let keys = [];
-    if(objectCount){
-        keys = await getAllObjectKeys(objectCount);
-    }
-    else{
-        keys = await getAllObjectKeys('ALL');
-    }
+    objectCount ? keys = await getAllObjectKeys(objectCount) : keys = await getAllObjectKeys('ALL');
+
     let data = [];
     getObjectByKey(keys[0]).then(obj => data.push(obj));
 

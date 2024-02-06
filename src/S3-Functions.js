6d208b1a5eecc99be45dc92d7f29b935630a72b0
Robert Recalo <rrecalo3@ggc.edu>
2024-02-06 17:22:18 -0500

    Fix graphql query issue causing old data to be presented as 'latest'

--- src/S3-Functions.js
+++ src/S3-Functions.js
@@ -41,7 +41,7 @@ export async function getLastDayData(){
         const yesterday = ((new Date() - (24 * 60 * 60 * 1000)) / 1000).toFixed();
         const result = await API.graphql(graphqlOperation(`
         query MyQuery {
-            listDht11Data(filter: {timestamp: {gt: ${yesterday}}}, limit: 2000) {
+            listDht11Data(filter: {timestamp: {gt: ${yesterday}}}, limit: 3500) {
                 items {
                   tempF
                   humidity
@@ -52,6 +52,8 @@ export async function getLastDayData(){
 
         let data = result.data.listDht11Data.items;
         data.sort((a, b) => a.timestamp - b.timestamp);
+        console.log(data[0]);
+        console.log(data[data.length-1]);
         return data;
     }
     catch(err){
